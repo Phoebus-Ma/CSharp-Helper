f2a946ab69199543854bf4b2f8a859e23e5e947c
Phoebus <tianjindahai@126.com>
2022-02-25 16:52:12 +0800

    [Desktop] Add user controls line chart sample.

---
+++ UserLineChart.WPF/UserLineChart/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserLineChart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool isExit = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UserLineChart_StartClick(object sender, RoutedEventArgs e)
+        {
+            int Value = 0;
+            Random random = new Random();
+            Point drawPoint = new Point();
+
+            isExit = false;
+
+            m_linechartShow.ClearPoint();
+
+            Task.Run(() =>
+            {
+                while (!isExit)
+                {
+                    Value = random.Next(0, 100);
+
+                    drawPoint = new Point(0, Value);
+
+                    m_linechartShow.Dispatcher.Invoke(() =>
+                    {
+                        m_linechartShow.AddPonit(1, drawPoint, m_linechartShow.Width);
+                    });
+
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+
+        private void UserLineChart_StopClick(object sender, RoutedEventArgs e)
+        {
+            isExit = true;
+        }
+    }
+}
---
+++ UserLineChart.WPF/UserLineChart/UserControls/ucLineChart.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserLineChart.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ucLineChart.xaml
+    /// </summary>
+    public partial class ucLineChart : UserControl
+    {
+        private double xPos = 0, xMove = 0;
+
+        public ucLineChart()
+        {
+            InitializeComponent();
+        }
+
+        public int AddPonit(int index, Point drawPoint, double areaWidth)
+        {
+            xPos += 2;
+            drawPoint.X = xPos;
+            xMove = drawPoint.X * (-1) + areaWidth - 2;
+            m_ucChartCanvas.Margin = new Thickness(xMove, 0, 0, 0);
+
+            if (0 == index)
+            {
+                m_ucChartPoly1.Points.Add(drawPoint);
+
+                if (drawPoint.X > areaWidth)
+                    m_ucChartPoly1.Points.RemoveAt(0);
+            }
+            else
+            {
+                m_ucChartPoly2.Points.Add(drawPoint);
+
+                if (drawPoint.X > areaWidth)
+                    m_ucChartPoly1.Points.RemoveAt(0);
+            }
+
+            return 0;
+        }
+
+        public int ClearPoint()
+        {
+            if (0 < m_ucChartPoly1.Points.Count)
+                m_ucChartPoly1.Points.Clear();
+
+            if (0 < m_ucChartPoly2.Points.Count)
+                m_ucChartPoly2.Points.Clear();
+
+            return 0;
+        }
+    }
+}
