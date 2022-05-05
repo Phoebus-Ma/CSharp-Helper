77ba12356912e24725f17ce47a3e1763028c84e6
Phoebus <tianjindahai@126.com>
2022-05-05 15:47:46 +0800

    [WPF] Add gzip compress example.

---
+++ Compress.WPF/Compress.GZIP/Compress.GZIP/GZipCompress.cs
@@ -0,0 +1,72 @@
+﻿/**
+ * Ref: [https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-compress-and-extract-files]
+ * 
+ * License - MIT.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Compress.GZIP
+{
+    public class GZipCompress
+    {
+        public static int Compress(string filename)
+        {
+            if (!File.Exists(filename))
+                return -1;
+
+            FileInfo fileToCompress = new FileInfo(filename);
+
+            if (".gz" == fileToCompress.Extension)
+                return 0;
+
+            using (FileStream originalFileStream = fileToCompress.OpenRead())
+            {
+                using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                {
+                    using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                        CompressionMode.Compress))
+                    {
+                        originalFileStream.CopyTo(compressionStream);
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public static int Decompress(string filename)
+        {
+            if (!File.Exists(filename))
+                return -1;
+
+            FileInfo fileToDecompress = new FileInfo(filename);
+
+            if (".gz" != fileToDecompress.Extension)
+                return -1;
+
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Compress.WPF/Compress.GZIP/Compress.GZIP/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Compress.GZIP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectSourceFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog srcFile = new OpenFileDialog();
+
+            srcFile.Filter = "All file|*.*";
+
+            if (true == srcFile.ShowDialog())
+            {
+                m_tbsource.Text = srcFile.FileName;
+            }
+        }
+
+        private void StartCompress_Click(object sender, RoutedEventArgs e)
+        {
+            if (m_tbsource.Text.Length < 0)
+            {
+                MessageBox.Show("Source path can not empty.");
+                return;
+            }
+
+            if (true == m_rbgzip.IsChecked)
+            {
+                GZipCompress.Compress(m_tbsource.Text);
+            }
+            else if (true == m_rbungzip.IsChecked)
+            {
+                GZipCompress.Decompress(m_tbsource.Text);
+            }
+        }
+    }
+}
