93b515b88c6bb765ecd91ad9f44016cbe00b98ed
Phoebus <tianjindahai@126.com>
2023-06-18 10:02:38 +0800

    [WPF] Add CPU Usage Monitor.

---
+++ Class-1/CPUUsageRecorder.WPF/CPUUsageRecorder/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPUUsageRecorder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Run(() =>
+            {
+                float usage = 0;
+                var cpuCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+
+                if (cpuCounter != null)
+                {
+                    while (true)
+                    {
+                        usage = cpuCounter.NextValue();
+
+                        tbl_usage.Dispatcher.Invoke(() =>
+                        {
+                            tbl_usage.Content = usage.ToString();
+                        });
+
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Class-1/CPUUsageRecorder.WPF/README.md
@@ -0,0 +1,9 @@
+
+## introduction
+
+Collect CPU load data using PerformanceCounter。
+
+## Tool
+
+Visual Studio 2022.
+.Net Framework 4.8.
