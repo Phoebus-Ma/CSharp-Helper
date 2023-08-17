e5502df94ed95ccab5986314761bfad7bdd92b00
Phoebus <tianjindahai@126.com>
2023-08-17 10:34:46 +0800

    [cs] Add hardware information example.

---
+++ Class-1/HardwareInformation.WPF/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Management;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HardwareInformation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Run(() =>
+            {
+                string infoString   = "";
+                string dispString   = "";
+
+                string cpuKeyClass  = "Win32_Processor";
+                string cpuKeyWord   = "Name,NumberOfCores,NumberOfLogicalProcessors,MaxClockSpeed";
+
+                string memKeyClass  = "Win32_PhysicalMemory";
+                string memKeyWord   = "BankLabel,ConfiguredClockSpeed,Capacity";
+
+                string gpuKeyClass  = "Win32_VideoController";
+                string gpuKeyWord   = "DeviceID,Name,AdapterRAM";
+
+                string diskKeyClass = "Win32_DiskDrive";
+                string diskKeyWord  = "Index,Model,Size";
+
+                GetHardwareInfo(cpuKeyClass, cpuKeyWord, out infoString);
+                dispString += "----CPU:-----\n"    + infoString + "\n";
+                GetHardwareInfo(memKeyClass, memKeyWord, out infoString);
+                dispString += "----Memory:----\n" + infoString + "\n";
+                GetHardwareInfo(gpuKeyClass, gpuKeyWord, out infoString);
+                dispString += "----GPU:----\n"    + infoString + "\n";
+                GetHardwareInfo(diskKeyClass, diskKeyWord, out infoString);
+                dispString += "----Disk:----\n"   + infoString;
+
+                lbl_info.Dispatcher.Invoke(() =>
+                {
+                    lbl_info.Content = dispString;
+                });
+            });
+        }
+
+        public static int GetHardwareInfo(string keyClass, string keyWord, out string infoString)
+        {
+            infoString = "";
+            string keyString = "SELECT " + keyWord + " FROM " + keyClass;
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(keyString);
+
+            string[] keys = keyWord.Split(new char[] { ',' });
+            foreach (ManagementObject info in searcher.Get())
+            {
+                foreach (string key in keys)
+                {
+                    infoString += key + ":\t" + info[key].ToString() + "\n";
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Class-1/HardwareInformation.WPF/README.md
@@ -0,0 +1,15 @@
+
+## Introduction
+
+The WMI service is used to obtain the hardware information.
+
+- Reference: [ManagementObjectSearcher.Get Method](https://learn.microsoft.com/en-us/dotnet/api/system.management.managementobjectsearcher.get?view=dotnet-plat-ext-7.0)
+
+- Reference: [Computer System Hardware Classes](https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes)
+
+
+## Platform
+
+- Windows 10+
+
+- Visual Studio 2022
